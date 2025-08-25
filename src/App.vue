4ed6e9cc894904f1c3068e53a20ea11a477b81bc
kimmh74 <zziipp.mw@gmail.com>
2025-08-25 14:48:49 +0900

    update
    1

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import MyHellow from './components/MyHellow.vue'
+</script>
+
+<template>
+    <HelloWorld msg="안녕 Vue야!!" />
+    <MyHellow />
+</template>
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    base: mode === 'production'  ?  '/vue_day01_0822/'  :  '/',       
+  }
+})
